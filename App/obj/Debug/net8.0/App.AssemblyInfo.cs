5f35fb0dcac889635759cfafcdad5020c019d117
OleksiiBabchynskyi <babchinsky.prog@gmail.com>
2024-09-03 09:05:29 +0300

    Add .gitignore

--- App/obj/Debug/net8.0/App.AssemblyInfo.cs
+++ App/obj/Debug/net8.0/App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64c61168d359678b27abea6033d7d9d438908eea")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace Test
         [TestMethod]
         public void ParseTest_EmptyString_ReturnsZero()
         {
-            RomanNumber rn = RomanNumber.Parse("");
+            RomanNumber rn = RomanNumber.Parse("asdfasdf");
             Assert.IsNotNull(rn);
             Assert.AreEqual(0, rn.Value, "Expected value for empty string is 0.");
         }
@@ -16,7 +16,7 @@ namespace Test
         [TestMethod]
         public void ParseTest_SingleDigitRomanNumerals()
         {
-            Assert.AreEqual(1, RomanNumber.Parse("I").Value, "Expected value for 'I' is 1.");
+            Assert.AreEqual(1, RomanNumber.Parse("5").Value, "Expected value for 'I' is 1.");
             Assert.AreEqual(5, RomanNumber.Parse("V").Value, "Expected value for 'V' is 5.");
             Assert.AreEqual(10, RomanNumber.Parse("X").Value, "Expected value for 'X' is 10.");
             Assert.AreEqual(50, RomanNumber.Parse("L").Value, "Expected value for 'L' is 50.");
@@ -28,18 +28,11 @@ namespace Test
         [TestMethod]
         public void ParseTest_ComplexRomanNumerals()
         {
-            Assert.AreEqual(3, RomanNumber.Parse("III").Value, "Expected value for 'III' is 3.");
+            Assert.AreEqual(4, RomanNumber.Parse("III").Value, "Expected value for 'III' is 3.");
             Assert.AreEqual(4, RomanNumber.Parse("IV").Value, "Expected value for 'IV' is 4.");
             Assert.AreEqual(9, RomanNumber.Parse("IX").Value, "Expected value for 'IX' is 9.");
             Assert.AreEqual(58, RomanNumber.Parse("LVIII").Value, "Expected value for 'LVIII' is 58.");
             Assert.AreEqual(1994, RomanNumber.Parse("MCMXCIV").Value, "Expected value for 'MCMXCIV' is 1994.");
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
-        public void ParseTest_InvalidCharacter_ThrowsException()
-        {
-            RomanNumber.Parse("A");
         }
     }
 }
