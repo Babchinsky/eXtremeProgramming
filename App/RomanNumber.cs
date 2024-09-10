04da8366bf84390f6c71f84eb04959d5452ccb10
OleksiiBabchynskyi <babchinsky.prog@gmail.com>
2024-09-10 08:01:11 +0300

    Homework

--- App/RomanNumber.cs
+++ App/RomanNumber.cs
@@ -1,47 +1,138 @@
-﻿namespace App
+﻿using System.Text;
+
+namespace App;
+
+public record RomanNumber(int Value)
 {
-    public record RomanNumber(int Value)
+    private readonly int _value = Value; // TODO: Refactoring - exclude
+
+    public int Value
     {
-        private readonly int _value = Value;
-        public int Value => _value;
+        get => _value;
+        init => _value = value;
+    }
 
-        public static RomanNumber Parse(string input)
+
+    public static RomanNumber Parse(String input)
+    {
+        int value = 0;
+        int prevDigit = 0; // TODO: rename to  ~rightDigit
+        int pos = input.Length;
+        List<String> errors = new();
+        foreach (char c in input.Reverse())
         {
-            if (string.IsNullOrWhiteSpace(input))
-                return new RomanNumber(0);
-
-            var numeralMap = new Dictionary<char, int>
+            pos -= 1;
+            int digit;
+            try
             {
-                {'I', 1},
-                {'V', 5},
-                {'X', 10},
-                {'L', 50},
-                {'C', 100},
-                {'D', 500},
-                {'M', 1000}
-            };
-
-            int total = 0;
-            int prevValue = 0;
-
-            foreach (char c in input.ToUpper())
+                digit = DigitalValue(c.ToString());
+            }
+            catch
             {
-                if (numeralMap.TryGetValue(c, out int value))
-                {
-                    if (value > prevValue)
-                        total += value - 2 * prevValue;
-                    else
-                        total += value;
-
-                    prevValue = value;
-                }
-                else
-                {
-                    throw new ArgumentException($"Invalid Roman numeral character: {c}");
-                }
+                errors.Add($"Invalid symbol '{c}' in position {pos}");
+                continue;
             }
 
-            return new RomanNumber(total);
+            if (digit != 0 && prevDigit / digit > 10)
+            {
+                errors.Add($"Invalid order '{c}' before '{input[pos + 1]}' in position {pos}");
+            }
+
+
+            value += digit >= prevDigit ? digit : -digit;
+            prevDigit = digit;
         }
+
+        if (errors.Any())
+        {
+            throw new FormatException(string.Join("; ", errors));
+        }
+
+        return new RomanNumber(value);
+    }
+
+
+    public static int DigitalValue(String digit)
+    {
+        return digit switch
+        {
+            "N" => 0,
+            "I" => 1,
+            "V" => 5,
+            "X" => 10,
+            "L" => 50,
+            "C" => 100,
+            "D" => 500,
+            "M" => 1000,
+            _ => throw new ArgumentException(
+                $" {nameof(RomanNumber)} : {nameof(DigitalValue)}: 'digit' has invalid value '{digit}'")
+        };
+    }
+
+    public RomanNumber Plus(RomanNumber other)
+    {
+        return this with { Value = Value + other.Value };
+    }
+
+    public RomanNumber Plus(string other)
+    {
+        return this with { Value = Value + Parse(other).Value };
+    }
+    /*ДЗ Скласти тести, що перевіряють роботу метода Plus
+     * з використанням римських записів чисел, наприклад
+     * IV + VI = X
+     */
+
+    public override string? ToString()
+    {
+        //3343->MMMCCCXLIII
+        // MMM
+        // D (500) X
+        // CD (400) X
+        // CCC
+        // LX
+        // III
+        if (_value == 0) return "N";
+        Dictionary<int, string> parts = new()
+        {
+            { 1000, "M" },
+            { 900, "CM" },
+            { 500, "D" },
+            { 400, "CD" },
+            { 100, "C" },
+            { 90, "XC" },
+            { 50, "L" },
+            { 40, "XL" },
+            { 10, "X" },
+            { 9, "IX" },
+            { 5, "V" },
+            { 4, "IV" },
+            { 1, "I" },
+        };
+
+
+        int v = _value;
+
+        StringBuilder sb = new();
+
+        foreach (var part in parts)
+        {
+            while (v >= part.Key)
+            {
+                v -= part.Key;
+                sb.Append(part.Value);
+            }
+        }
+
+        return sb.ToString();
     }
 }
+
+
+/* Повторити основи управління програмними проєктами
+ * Закласти рішення (solution) з двома проєктами (App, Test)
+ * Налаштувати запуск тестів
+ * *Реалізувати парсер одиночних цифр римських чисел, додати тести до них
+ * Форми здачі:
+ * -архів з проєктом та скріншотами або посилання на github
+ */
